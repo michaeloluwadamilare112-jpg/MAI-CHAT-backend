0549d82544052984abae1b014fad8fd8480b65f6
michaeloluwadamilare112-jpg <michaeloluwadamilare112@gmail.com>
2026-04-28 21:39:28 +0100

    Delete maichat-backend/modelss directory

--- maichat-backend/modelss/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("User", userSchema);
