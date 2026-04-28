fc45c6b7d395fa971063d531c7dacd3029ab0a3d
michaeloluwadamilare112-jpg <michaeloluwadamilare112@gmail.com>
2026-04-28 21:39:17 +0100

    Delete maichat-backend/routes directory

--- maichat-backend/routes/chat.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handleChat } from "../controllers/chatController.js";
-
-const router = express.Router();
-
-router.post("/", handleChat);
-
-export default router;
